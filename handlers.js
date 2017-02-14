c5efe23ff19211250c866a2b878c7d982fb75892
Alexander Gudulin <alexandr.gudulin@gmail.com>
2017-02-14 10:40:44 +0100

    :zap: cache the posters

--- handlers.js
+++ handlers.js
@@ -2,7 +2,9 @@ const stream = require('send')
 
 const { parseFullSeason } = require('./grabber')
 
-const getPoster = ({ req, res, params: { season, episode } }) => stream(req, `./data/${season}/${episode}.jpg`).pipe(res)
+const getPoster = ({ req, res, params: { season, episode } }) => (
+  stream(req, `./data/${season}/${episode}.jpg`, { maxAge: '1y' }).pipe(res)
+)
 const grabSeason = ({ params: { season } }) => parseFullSeason(season)
 const getSeason = ({ params: { season } }) => require(`./data/${season}/data.json`)
 const noop = () => {}
